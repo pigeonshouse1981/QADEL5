b5df5b519d58ad6a8cb74ff55d954cd2de1be2e4
Aron Fielder <pigeonshouse@gmail.com>
2025-07-29 13:49:10 +0100

    29 July 2025 13:49

---
+++ unit_test_demo/test_l1.py
@@ -0,0 +1,9 @@
+import unittest
+from calculator import Calculator
+
+class TestOperations(unittest.TestCase):
+
+    def test_sum(self)
+        calculation = Calculator(2,2)
+        answer = Calculator.get_sum
+        self.assertEqual(answer, 4, 'The sum is wrong. ')
